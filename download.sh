ad4673e4a7e3fcce04f5b191f86171b6a371df7f
Marius Gedminas <marius@gedmin.as>
2014-10-24 08:45:35 +0300

    Verify all the SHA256SUMs

--- download.sh
+++ download.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 # adjust as desired, the URL scheme should be clear
 
-## wget -c http://lt.releases.ubuntu.com/precise/SHA256SUMS -O SHA256SUMS.precise
-## wget -c http://lt.releases.ubuntu.com/precise/SHA256SUMS.gpg -O SHA256SUMS.precise.gpg
+wget -c http://lt.releases.ubuntu.com/precise/SHA256SUMS -O SHA256SUMS.precise
+wget -c http://lt.releases.ubuntu.com/precise/SHA256SUMS.gpg -O SHA256SUMS.precise.gpg
 ## wget -c http://lt.releases.ubuntu.com/precise/ubuntu-12.04.4-server-amd64.iso
 ## wget -c http://lt.releases.ubuntu.com/precise/ubuntu-12.04.4-server-i386.iso
 ## wget -c http://lt.releases.ubuntu.com/precise/ubuntu-12.04.4-desktop-amd64.iso
 ## wget -c http://lt.releases.ubuntu.com/precise/ubuntu-12.04.4-desktop-i386.iso
 
-## wget -c http://lt.releases.ubuntu.com/trusty/SHA256SUMS -O SHA256SUMS.trusty
-## wget -c http://lt.releases.ubuntu.com/trusty/SHA256SUMS.gpg -O SHA256SUMS.trusty.gpg
+wget -c http://lt.releases.ubuntu.com/trusty/SHA256SUMS -O SHA256SUMS.trusty
+wget -c http://lt.releases.ubuntu.com/trusty/SHA256SUMS.gpg -O SHA256SUMS.trusty.gpg
 ## wget -c http://lt.releases.ubuntu.com/trusty/ubuntu-14.04-desktop-amd64.iso
 ## wget -c http://lt.releases.ubuntu.com/trusty/ubuntu-14.04-server-amd64.iso
 
 ## for ubuntu-gnome: http://cdimage.ubuntu.com/ubuntu-gnome/releases/14.10/release/
 wget -c http://cdimage.ubuntu.com/ubuntu-gnome/releases/14.10/release/SHA256SUMS -O SHA256SUMS.utopic
-wget -c http://cdimage.ubuntu.com/ubuntu-gnome/releases/14.10/release/SHA256SUMS.gpg -O SHA256SUMS.utopic.gpg 
-wget -c http://cdimage.ubuntu.com/ubuntu-gnome/releases/14.10/release/ubuntu-gnome-14.10-desktop-amd64.iso
+wget -c http://cdimage.ubuntu.com/ubuntu-gnome/releases/14.10/release/SHA256SUMS.gpg -O SHA256SUMS.utopic.gpg
+## wget -c http://cdimage.ubuntu.com/ubuntu-gnome/releases/14.10/release/ubuntu-gnome-14.10-desktop-amd64.iso
--- verify.sh
+++ verify.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+# http://askubuntu.com/a/449483
 #VERIFY="gpg --verify"
 VERIFY="gpgv --keyring=/usr/share/keyrings/ubuntu-archive-keyring.gpg"
 
-#$VERIFY SHA256SUMS.precise.gpg SHA256SUMS.precise
-#grep 'desktop\|server' SHA256SUMS.precise | sha256sum -c -
-
+$VERIFY SHA256SUMS.precise.gpg SHA256SUMS.precise
+$VERIFY SHA256SUMS.trusty.gpg SHA256SUMS.trusty
 $VERIFY SHA256SUMS.utopic.gpg SHA256SUMS.utopic
-grep 'amd64' SHA256SUMS.utopic | sha256sum -c -
+
+#grep 'desktop\|server' SHA256SUMS.precise | sha256sum -c -
+#grep 'amd64' SHA256SUMS.trusty | sha256sum -c -
+#grep 'amd64' SHA256SUMS.utopic | sha256sum -c -
