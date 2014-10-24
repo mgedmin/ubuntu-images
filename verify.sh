eb63cd46a4982fc1b9cbc34f35dd1b283694bf45
Marius Gedminas <marius@gedmin.as>
2014-10-24 08:40:22 +0300

    Download Ubuntu GNOME 14.10.
    Use http://askubuntu.com/a/449483 to verify GPG signatures.

--- verify.sh
+++ verify.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-gpg --verify SHA256SUMS.precise.gpg SHA256SUMS.precise
-grep 'desktop\|server' SHA256SUMS.precise | sha256sum -c -
+#VERIFY="gpg --verify"
+VERIFY="gpgv --keyring=/usr/share/keyrings/ubuntu-archive-keyring.gpg"
+
+#$VERIFY SHA256SUMS.precise.gpg SHA256SUMS.precise
+#grep 'desktop\|server' SHA256SUMS.precise | sha256sum -c -
+
+$VERIFY SHA256SUMS.utopic.gpg SHA256SUMS.utopic
+grep 'amd64' SHA256SUMS.utopic | sha256sum -c -
